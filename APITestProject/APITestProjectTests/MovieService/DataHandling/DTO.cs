310166603fa48caf7a99296dfbeb2afd409dcbe3
Benjamin <h8bmf@hotmail.co.uk>
2021-09-15 17:09:08 +0100

    Added Framework - untested

---
+++ APITestProject/APITestProjectTests/MovieService/DataHandling/DTO.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace APITestProjectTests.MovieService.DataHandling
+{
+    public class DTO<ResponseType> where ResponseType : IResponse, new()
+    {
+        public ResponseType Response { get; set; }
+        public void DeserializeResponse(string movieResponse)
+        {
+            Response = JsonConvert.DeserializeObject<ResponseType>(movieResponse);
+        }
+    }
+}
---
+++ APITestProject/APITestProjectTests/MovieService/MovieService.cs
@@ -0,0 +1,29 @@
+﻿using APITestProjectTests.MovieService.DataHandling;
+using APITestProjectTests.MovieService.Manager;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace APITestProjectTests.MovieService
+{
+    public class MovieService
+    {
+        public CallManager callManager { get; set; }
+        public JObject Json_response { get; set; }
+        public DTO<MovieResponse> MovieResponseDTO { get; set; }
+        public string MovieSelected { get; set; }
+        public string MovieResponse { get; set; }
+        public MovieService()
+        {
+            callManager = new CallManager();
+            MovieResponseDTO = new DTO<MovieResponse>();
+        }
+        public async Task MakeRequestAsync(string movie)
+        {
+            MovieSelected = movie;
+            MovieResponse = await callManager.MakeMovieRequestAsync(movie);
+            Json_response = JObject.Parse(MovieResponse);
+            MovieResponseDTO.DeserializeResponse(MovieResponse);
+        }
+        // Add Extra functionality to make sure things are there, etc.
+    }
+}
