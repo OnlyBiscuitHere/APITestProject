ff171cab897f539b5c5c4d516d4f9dfd80252bd5
Benjamin <h8bmf@hotmail.co.uk>
2021-09-16 11:55:30 +0100

    Tests solution added

---
+++ APITestProjectTests/MovieIOService/HTTPManager/CallManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace APITestProjectTests.MovieService.HTTPManager
+{
+    public class CallManager
+    {
+        private readonly IRestClient _client;
+        public int StatusCode { get; set; }
+        public CallManager()
+        {
+            _client = new RestClient(AppConfigReader.BaseUrl);
+        }
+        public async Task<string> MakeMovieRequestAsync(string movie)
+        {
+            var request = new RestRequest();
+            SpacerMovie(movie);
+            request.Resource = $"?t={movie}&apikey=6d25abe0";
+            IRestResponse response = await _client.ExecuteAsync(request);
+            StatusCode = (int)response.StatusCode;
+            return response.Content;
+        }
+        public void SpacerMovie(string movie)
+        {
+            if (movie.Contains(" "))
+            {
+                movie.Replace(' ', '+');
+            }
+        }
+    }
+}
---
+++ APITestProjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITestProjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APITestProjectTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92a69169-2873-4f15-85de-7664176c1f93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
