e939a10f7b643794a01cc06bf9d43e26d9627954
Sérgio Pessegueiro <55961303+xCardinal@users.noreply.github.com>
2021-09-16 11:21:35 +0100

    Deleted the main program.

--- APITestProject/APITestProjectTests/AppConfigReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Configuration;
-
-namespace APITestProjectTests
-{
-    public class AppConfigReader
-    {
-        public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
-    }
-}
