0869ff1557b6d5ad2c4ec121d680b4afde490289
Sérgio Pessegueiro <55961303+xCardinal@users.noreply.github.com>
2021-09-16 11:27:42 +0100

    Add a new empty console application.

---
+++ APITestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APITestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
