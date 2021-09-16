8d252fc11a75281bf5e2bd604026e03a47be0ef7
Sérgio Pessegueiro <55961303+xCardinal@users.noreply.github.com>
2021-09-16 12:11:00 +0100

    Ben check this

--- APITestProject/Program.cs
+++ APITestProject/Program.cs
@@ -2,11 +2,20 @@
 
 namespace APITestProject
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            MyMethod("hey");
+
+        }
+
+        public static string MyMethod(string blabla)
+        {
+
+            return String.Empty;
         }
     }
 }
