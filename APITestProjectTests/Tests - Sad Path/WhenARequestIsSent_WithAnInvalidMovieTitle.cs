535dfcf1ee278ec04d8d9422bba773ef51468044
michael carini <snownkc@icloud.com>
2021-09-16 15:04:53 +0100

    added sad path, tested

---
+++ APITestProjectTests/Tests - Sad Path/WhenARequestIsSent_WithAnInvalidMovieTitle.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using APITestProjectTests.MovieIOService;
+using System.Threading.Tasks;
+
+namespace APITestProjectTests.Tests___Sad_Path
+{
+    public class WhenARequestIsSentWithAnInvalidMovieTitle
+    {
+        private MovieServices _movieServices;
+        [OneTimeSetUp]
+        public async Task OneTimeSetup()
+        {
+            _movieServices = new MovieServices();
+            await _movieServices.MakeAMovieRequestAsync("idk");
+        }
+
+        [Test]
+        public void GivenAnInvalidMovieTitle_ResponseIsFalse()
+        {
+            Assert.That(_movieServices.MovieResponseDTO.Response.Title, Is.EqualTo(null));
+        }
+
+        //[Test]
+        //public void GivenAnInvalidMovieTitle_StatusCodeIs404()
+        //{
+        //    Assert.That(_movieServices.CallManager.StatusCode, Is.EqualTo("404"));
+        //}
+
+    }
+}
